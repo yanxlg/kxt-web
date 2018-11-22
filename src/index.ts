faa5c6e65e02be658371a3c993874439f415b0be
严贤良 <yanxianliang@codyy.com>
2018-11-22 17:28:59 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export {default as Tab} from './tab';
-export {default as ImageCrop} from './crop';
+export {default as ImageCrop} from './imagecrop';
 export {default as Player} from './player';
 export {default as Loading} from "./loading";
 export {default as connect} from "./connect";
