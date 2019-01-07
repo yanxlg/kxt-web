090213944a04cb3ef747aebc11fb6915ec642e5f
yanxianliang <798410225@qq.com>
2019-01-07 12:06:24 +0800

    fix

---
+++ src/perfectscrollbar/perfectscrollbarfactory.ts
@@ -0,0 +1,164 @@
+/**
+ * @Author: yanxinaliang (rainyxlxl@163.com)
+ * @Date: 2019/1/7 12:04
+ * @Last Modified by: yanxinaliang (rainyxlxl@163.com)
+ * @Last Modified time: 2019/1/7 12:04
+ * @disc:factory 脱离react
+ */
+
+import {default as _PerfectScrollbar} from './index.lib';
+import {Spring, SpringSystem} from 'rebound';
+import {Bind, Debounce, Throttle} from 'lodash-decorators';
+import {IScrollFactoryOptions} from './index';
+import "./css/style.less";
+import "./css/perfect-scrollbar.css";
+
+class PerfectScrollbarFactory extends _PerfectScrollbar{
+    private horizonSpringSystem: SpringSystem;
+    private verticalSpringSystem: SpringSystem;
+    private horizonSpring: Spring;
+    private verticalSpring: Spring;
+    private _disabled:boolean;
+    public get disabled(): boolean {
+        return this._disabled;
+    }
+    public set disabled(value: boolean) {
+        this._disabled = value;
+        // 隐藏滚动条
+        if(value){
+            this.element.classList.add("ps-scroll-disable");
+        }else{
+            this.element.classList.remove("ps-scroll-disable");
+        }
+    }
+    constructor(element:HTMLElement,setting: IScrollFactoryOptions){
+        super(element,setting);
+        this.disabled=setting.disabled;
+        this.horizonSpringSystem = new SpringSystem();
+        this.verticalSpringSystem = new SpringSystem();
+        this.horizonSpring = this.horizonSpringSystem.createSpring();
+        this.verticalSpring = this.verticalSpringSystem.createSpring();
+        this.horizonSpring.addListener(
+            {onSpringUpdate: this.handleHorizonSpringUpdate});
+        this.verticalSpring.addListener(
+            {onSpringUpdate: this.handleVerticalSpringUpdate});
+        
+        this.element.addEventListener("scroll",this.handleScroll);
+        window.addEventListener('resize', this.handleWindowResize);
+    }
+    
+    @Bind
+    @Debounce(500)
+    private handleWindowResize() {
+        this.update();
+    }
+    
+    @Bind
+    private handleScroll(){
+        this.handleScrollUpdate();
+        this.handleScrollEnd();
+    }
+    
+    @Bind
+    @Throttle(300)
+    private handleScrollUpdate(){
+        this.update();
+    }
+    
+    @Bind
+    @Debounce(500)
+    private handleScrollEnd(){
+        let event:any=document.createEvent("HTMLEvents");
+        event.initEvent("ps-scroll-end",false,false);
+        this.element.dispatchEvent(event);
+    }
+    
+    @Bind
+    private handleHorizonSpringUpdate(spring: Spring) {
+        const val = spring.getCurrentValue();
+        this.scrollLeft(val, false);
+    }
+    
+    @Bind
+    private handleVerticalSpringUpdate(spring: Spring) {
+        const val = spring.getCurrentValue();
+        this.scrollTop(val, false);
+    }
+    
+    @Bind
+    public update() {
+        super.update();
+    }
+    
+    @Bind
+    public destroy() {
+        this.element.removeEventListener("scroll",this.handleScroll);
+        window.removeEventListener('resize', this.handleWindowResize);
+        this.horizonSpringSystem.deregisterSpring(this.horizonSpring);
+        this.horizonSpringSystem.removeAllListeners();
+        this.horizonSpringSystem = undefined;
+        this.horizonSpring.destroy();
+        this.horizonSpring = undefined;
+        this.verticalSpringSystem.deregisterSpring(this.verticalSpring);
+        this.verticalSpringSystem.removeAllListeners();
+        this.verticalSpringSystem = undefined;
+        this.verticalSpring.destroy();
+        this.verticalSpring = undefined;
+        super.destroy();
+    }
+    
+    @Bind
+    public scrollLeft(left: number, animation?: boolean) {
+        if (!this.element) return;
+        if (animation) {
+            const {scrollLeft} = this.element;
+            this.verticalSpring.setCurrentValue(scrollLeft).setAtRest();
+            this.verticalSpring.setEndValue(left);
+        } else {
+            this.element.scrollLeft = left;
+        }
+    }
+    
+    @Bind
+    public scrollTop(top: number, animation?: boolean) {
+        if (!this.element) return;
+        if (animation) {
+            const {scrollTop} = this.element;
+            // const val = MathUtil.mapValueInRange(top, 0, scrollHeight,
+            //     scrollHeight * 0.2, scrollHeight * 0.8);
+            this.verticalSpring.setCurrentValue(scrollTop).setAtRest();
+            this.verticalSpring.setEndValue(top);
+        } else {
+            this.element.scrollTop = top;
+        }
+    }
+    
+    @Bind
+    public scrollToLeft(animation?: boolean) {
+        if (!this.element) return;
+        this.scrollLeft(0, animation);
+    }
+    
+    @Bind
+    public scrollToTop(animation?: boolean) {
+        if (!this.element) return;
+        this.scrollTop(0, animation);
+    }
+    
+    @Bind
+    public scrollToRight(animation?: boolean) {
+        if (!this.element) return;
+        const {scrollWidth, offsetWidth} = this.element;
+        this.scrollLeft(scrollWidth - offsetWidth, animation);
+    }
+    
+    @Bind
+    public scrollToBottom(animation?: boolean) {
+        if (!this.element) return;
+        const {scrollHeight, offsetHeight} = this.element;
+        this.scrollTop(scrollHeight - offsetHeight, animation);
+    }
+    
+}
+
+export {PerfectScrollbarFactory};
