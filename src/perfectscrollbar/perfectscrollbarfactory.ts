a720970b3e893472f0a7b270d7410d056b9634f5
yanxianliang <798410225@qq.com>
2019-04-07 17:32:26 +0800

    fix

--- src/perfectscrollbar/perfectscrollbarfactory.ts
+++ src/perfectscrollbar/perfectscrollbarfactory.ts
@@ -5,14 +5,13 @@
  * @Last Modified time: 2019/1/7 12:04
  * @disc:factory 脱离react
  */
-
+import {default as _PerfectScrollbar} from './index.lib';
 import {Spring, SpringSystem} from 'rebound';
 import {Bind, Debounce, Throttle} from 'lodash-decorators';
 import "./css/style.less";
 import "./css/perfect-scrollbar.css";
 import {CSSProperties, MouseEventHandler, RefObject} from 'react';
 
-const _PerfectScrollbar = require("./index.lib.js");
 
 export type handler =
     'click-rail'
